7e377486f4320640306e26385665278c1674b152
Jimmy Charnley Kromann <jimmy.kromann@novartis.com>
2023-12-11 15:38:02 +0100

    init

--- src/hpce_utils/__init__.py
+++ src/hpce_utils/__init__.py
@@ -1,13 +1,2 @@
-from hpc_env import lmod, uge
-from hpc_env.utils import (
-    get_available_cores,
-    get_shm_path,
-    get_threads,
-    is_notebook,
-    is_uge,
-    set_threads,
-    which,
-)
-from hpc_env.version import VERSION
-
+from hpce_utils.version import VERSION
 __version__ = VERSION
