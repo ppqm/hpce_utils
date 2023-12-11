c2a2a7683262ebcb3442f5682b9ee35f1cf5d74f
Jimmy Charnley Kromann <jimmy.kromann@novartis.com>
2023-12-11 16:03:49 +0100

    init

--- tests/test_lmod.py
+++ tests/test_lmod.py
@@ -1,15 +1,16 @@
 import os
-import sys
+
 import pytest
 
 from hpce_utils.env import lmod
 
-if lmod.get_lmod_executable():
+if not lmod.get_lmod_executable():
     pytest.skip("Could not find LMOD executable", allow_module_level=True)
+
 
 def test_use() -> None:
 
-    module_path = "GET RESOURCES PATH" # TODO
+    module_path = "GET RESOURCES PATH"  # TODO
     lmod.use(module_path)
     assert os.environ.get("MODULEPATH") is not None
     assert module_path in os.environ.get("MODULEPATH", "")
